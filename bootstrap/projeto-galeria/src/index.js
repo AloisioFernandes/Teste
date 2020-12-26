96e3e1290f27ce81968d7c2bb1b430fcc1f39afd
AloisioFernandes <aloisio.fernandes@outlook.com>
2020-12-26 14:13:10 -0300

    Carregando HTML via Ajax.

--- bootstrap/projeto-galeria/src/index.js
+++ bootstrap/projeto-galeria/src/index.js
@@ -1,2 +1,9 @@
 //Arquivos SASS
 import './scss/index.scss'
+
+//Dependências para estarem presentes no arquivo final por webpack
+import 'jquery'
+import 'bootstrap'
+
+//Arquivos Javascript
+import './js/core/includes'
