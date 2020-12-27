c986815b2938a0f2bed9fdc443d89dfc1f7de109
AloisioFernandes <aloisio.fernandes@outlook.com>
2020-12-27 00:54:26 -0300

    Componentes Pai e Filho 2.

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Pai from './components/Pai'
+import Filho from './components/Filho'
 
 ReactDOM.render(
   <div>
     <Pai nome="Paulo" sobrenome="Silva">
-      
+      <Filho nome="Pedro" />
+      <Filho nome="Paulo" />
+      <Filho nome="Carla" />
     </Pai>
   </div>
 , document.getElementById('root'))
