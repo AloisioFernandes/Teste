e2612d1b85eaec0d50da5aa6773f6c8ffeb8af40
AloisioFernandes <aloisio.fernandes@outlook.com>
2020-12-27 00:08:41 -0300

    Múltiplos componentes.

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import BomDia from './components/BomDia'
+import Multi, { BoaNoite } from './components/Multiplos'
 
-ReactDOM.render(<BomDia nome="Guilherme" />, document.getElementById('root'))
+ReactDOM.render(
+  <div>
+    <Multi.BoaTarde nome="Ana" />
+    <BoaNoite nome="Bia" />
+  </div>
+, document.getElementById('root'))
