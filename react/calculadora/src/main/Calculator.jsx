0e37ba28b741dd1f4b9121e1ebeee971e0a8103a
AloisioFernandes <aloisio.fernandes@outlook.com>
2020-12-27 22:49:18 -0300

    Componente botão 2.

--- react/calculadora/src/main/Calculator.jsx
+++ react/calculadora/src/main/Calculator.jsx
@@ -6,26 +6,38 @@ import Button from '../components/Button'
 import Display from '../components/Display'
 
 export default function Calculator() {
+  function clearMemory() {
+    console.log('limpar')
+  }
+
+  function setOperation(operation){
+    console.log(operation)
+  }
+
+  function addDigit(n){
+    console.log(n)
+  }
+
   return (
     <div className="calculator">
       <Display value={100} />
-      <Button label="AC" />
-      <Button label="/" />
-      <Button label="7" />
-      <Button label="8" />
-      <Button label="9" />
-      <Button label="*" />
-      <Button label="4" />
-      <Button label="5" />
-      <Button label="6" />
-      <Button label="-" />
-      <Button label="1" />
-      <Button label="2" />
-      <Button label="3" />
-      <Button label="+" />
-      <Button label="0" />
-      <Button label="." />
-      <Button label="=" />
+      <Button label="AC" click={clearMemory} triple />
+      <Button label="/" click={(op) => setOperation(op)} operation />
+      <Button label="7" click={(n) => addDigit(n)} />
+      <Button label="8" click={(n) => addDigit(n)} />
+      <Button label="9" click={(n) => addDigit(n)} />
+      <Button label="*" click={(op) => setOperation(op)} operation />
+      <Button label="4" click={(n) => addDigit(n)} />
+      <Button label="5" click={(n) => addDigit(n)} />
+      <Button label="6" click={(n) => addDigit(n)} />
+      <Button label="-" click={(op) => setOperation(op)} operation />
+      <Button label="1" click={(n) => addDigit(n)} />
+      <Button label="2" click={(n) => addDigit(n)} />
+      <Button label="3" click={(n) => addDigit(n)} />
+      <Button label="+" click={(op) => setOperation(op)} operation />
+      <Button label="0" click={(n) => addDigit(n)} double />
+      <Button label="." click={(n) => addDigit(n)} />
+      <Button label="=" click={(op) => setOperation(op)} operation />
     </div>
   )
 }
