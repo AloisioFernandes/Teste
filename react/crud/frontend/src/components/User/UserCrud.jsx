d578021f5e315ba921b59f33f5c92bb1ad0c7a31
AloisioFernandes <aloisio.fernandes@outlook.com>
2020-12-28 23:51:34 -0300

    Componente Cadastro de Usuário 2.

--- react/crud/frontend/src/components/User/UserCrud.jsx
+++ react/crud/frontend/src/components/User/UserCrud.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Main from '../template/Main'
 
+const baseUrl = 'http://localhost:3001/users'
 const headerProps = {
   icon: 'users',
   title: 'Usuários',
@@ -8,6 +10,32 @@ const headerProps = {
 }
 
 const UserCrud = () => {
+  const [user, setUser] = useState({name: '', email: ''})
+  const [list, setList] = useState([])
+  
+  function handleClear() {
+    setUser({name: '', email: ''})
+  }
+
+  function getUpdatedList(user) {
+    const lista = list.filter(u => u.id !== user.id)
+    lista.unshift(user)
+    return lista
+  }
+  
+  function handleSave() {
+    const usr = user
+    const method = usr.id ? 'put' : 'post' //se tiver id fará requisição put, se não será post
+    const url = usr.id ? `${baseUrl}/${user.id}` : baseUrl 
+    axios[method](url, usr)
+      .then(resp => {
+        const lst = getUpdatedList(resp.data)
+        setUser({name: '', email: ''})
+        setList(lst)
+      })
+  }
+
+
   return (
     <Main {...headerProps}>
       Cadastro de Usuário
