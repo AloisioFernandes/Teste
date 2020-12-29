d215663e6cb2d7a819d356fca30b5602cc54a0ac
AloisioFernandes <aloisio.fernandes@outlook.com>
2020-12-28 22:02:19 -0300

    Componente Conteúdo.

--- react/crud/frontend/src/components/template/Main.jsx
+++ react/crud/frontend/src/components/template/Main.jsx
@@ -5,8 +5,10 @@ import Header from './Header'
 export default (props) => (
   <>
     <Header {...props} /> {/*Header recebe as mesmas propriedades de Main*/}
-    <main className="content">
-      Conteúdo
+    <main className="content container-fluid">
+      <div className="p-3 mt-3">
+        {props.children}
+      </div>
     </main>
   </>
 )
