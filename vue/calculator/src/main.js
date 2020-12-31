0e22c487cd3056d9679caee15f3a573acf71954a
AloisioFernandes <aloisio.fernandes@outlook.com>
2020-12-31 20:22:35 -0300

    Início do projeto.

---
+++ vue/calculator/src/main.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import App from './App'
+
+new Vue({
+  render: h => h(App) //irá renderizar um componente Vue
+}).$mount("#app") //id do elemento que terá a aplicação inserida
