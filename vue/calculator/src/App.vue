96870296d9c7426cc3cd9d9f85d4960099e91e06
AloisioFernandes <aloisio.fernandes@outlook.com>
2020-12-31 20:43:30 -0300

    Componentes.

--- vue/calculator/src/App.vue
+++ vue/calculator/src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
-    <h1>Olá Vue!</h1>
+    <h1>Calculadora</h1>
+    <Calculator />
   </div>
 </template>
 
 <script>
-export default {
+import Calculator from './main/Calculator'
 
+export default {
+  components: { Calculator } //exportando componente para utilizá-lo no template
 }
 </script>
 
---
+++ vue/calculator/src/main/Calculator.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="calculator">
+    <Display />
+    <Button />
+    <Button />
+    <Button />
+  </div>
+</template>
+
+<script>
+import Button from '../components/Button'
+import Display from '../components/Display'
+
+export default {
+  components: { Button, Display } //exportando componentes para serem utilizados no template
+}
+</script>
+
+<style>
+.calculator {
+  height: 320px;
+  width: 235px;
+  border-radius: 5px;
+  overflow: hidden;
+
+  display: grid;
+  grid-template-columns: repeat(4, 25%);
+  grid-template-rows: 1fr repeat(5, 48px);
+}
+</style>
