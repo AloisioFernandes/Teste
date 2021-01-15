fdf4d689290449f7623101d5c770761045bedcab
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-01-15 18:06:12 -0300

    Inserir prefeitos.

---
+++ banco-relacional/inserir_prefeitos.sql
@@ -0,0 +1,11 @@
+-- INSERT INTO prefeitos (nome, cidade_id)
+-- VALUES 
+--   ('Rodrigo Neves', 3),
+--   ('Raquel Lyra', 4),
+--   ('Zenaldo Coutinho', NULL);
+
+INSERT INTO prefeitos (nome, cidade_id)
+VALUES 
+  ('Rafael Greca', NULL);
+
+SELECT * FROM prefeitos;
