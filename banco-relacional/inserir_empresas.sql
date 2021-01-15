3fc957d66f077d4e522c93b6611da0ba6eb5af95
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-01-15 18:41:30 -0300

    Inserir empresas.

---
+++ banco-relacional/inserir_empresas.sql
@@ -0,0 +1,12 @@
+-- ALTER TABLE empresas MODIFY cnpj VARCHAR(14);
+
+-- INSERT INTO empresas (nome, cnpj)
+-- VALUES 
+--   ('Bradesco', 95694186000132),
+--   ('Vale', 27887148000146),
+--   ('Cielo', 01598317000134);
+
+  DESC empresas;
+  DESC prefeitos;
+  SELECT * FROM empresas;
+  SELECT * FROM cidades;
