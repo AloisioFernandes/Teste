7664c66db33a2cb169d249f2e41976af686bedd3
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-01-15 18:48:42 -0300

    Inserir empresas.

--- banco-relacional/inserir_empresas.sql
+++ banco-relacional/inserir_empresas.sql
@@ -6,7 +6,14 @@
 --   ('Vale', 27887148000146),
 --   ('Cielo', 01598317000134);
 
-  DESC empresas;
-  DESC prefeitos;
-  SELECT * FROM empresas;
-  SELECT * FROM cidades;
+-- DESC empresas;
+-- DESC prefeitos;
+-- SELECT * FROM empresas;
+-- SELECT * FROM cidades;
+
+INSERT INTO `empresas_unidades` (empresa_id, cidade_id, sede)
+VALUES 
+  (1, 1, 1),
+  (1, 2, 0),
+  (2, 1, 0),
+  (2, 2, 1);
