5c8eb49d9f807470275ebd759d9acb7cc678cc6b
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-02-06 19:52:53 -0300

    Interface gráfica da lista de usuários.

--- rncrud/src/views/UserList.js
+++ rncrud/src/views/UserList.js
@@ -1,8 +1,60 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Alert } from 'react-native'
+import { View, FlatList } from 'react-native'
+import { Avatar, Button, ListItem, Icon } from 'react-native-elements'
+import users from '../data/users'
 
 export default (props) => {
+
+  function confirmUserDeletion(user) {
+    Alert.alert('Excluir Usuário', 'Deseja excluir o usuário?', [
+      { text: 'Sim', onPress(){console.warn('delete' + user.id)} },
+      { text: 'Não' }
+    ])
+  }
+
+  function getActions(user) {
+    return ([
+      <>
+        <Button
+          onPress={() => props.navigation.navigate('UserForm', user)} // Passa usuário selecionado para próxima rota
+          type="clear"
+          icon={<Icon name="edit" size={25} color="orange" />}
+        />  
+        <Button
+          onPress={() => confirmUserDeletion(user)} // confirma se usuário deve ser deletado 
+          type="clear"
+          icon={<Icon name="delete" size={25} color="red" />}
+        />  
+      </>
+    ])
+  }
+
+  function getUserItem({ item: user }) { // cada item do array de users será reconhecido como user
+    return (
+      <ListItem 
+        key={user.id} 
+        bottomDivider
+        rightElement={getActions(user)}
+        onPress={() => props.navigation.navigate('UserForm', user)} // Passa usuário selecionado para próxima rota
+      >
+        <Avatar source={{ uri: user.avatarUrl }} />
+        <ListItem.Content>
+          <ListItem.Title>{user.name}</ListItem.Title>
+          <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+        </ListItem.Content>
+          <ListItem.ButtonGroup containerStyle={{borderColor:'rgba(255,255,255, 0)', flexDirection:'row', maxWidth: '26%', marginRight: -15}} buttons={getActions(user)}/>
+      </ListItem>
+    )
+  }
+
   return (
-    <Text>UserList</Text>
+    <View>
+      <FlatList
+        keyExtractor={user => user.id.toString()}
+        data={users}
+        renderItem={getUserItem}
+      />
+    </View>
   )
 }
