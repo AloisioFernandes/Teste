cba40dcaa4302acabb29066018035b259919729b
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-02-06 20:03:11 -0300

    Formulário iniciado.

--- rncrud/src/views/UserForm.js
+++ rncrud/src/views/UserForm.js
@@ -1,8 +1,16 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useState } from 'react'
+import { Text, View, TextInput } from 'react-native'
 
-export default (props) => {
+export default ({ route, navigation }) => {
+  const [user, setUser] = useState(route.params ? route.params : {}) // verifica se route.params existe, se usuário entrou para editar ou adicionar usuário
   return (
-    <Text>UserForm</Text>
+    <View>
+      <Text>Name</Text>
+      <TextInput 
+        onChangeText={name => setUser({ ...user, name})} // atualiza o nome do usuário
+        placeholder="Informe o Nome"
+        value={user.name}
+      />
+    </View>
   )
 }
