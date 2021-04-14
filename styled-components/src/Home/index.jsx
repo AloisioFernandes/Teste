9aa77c7314ed758c1b22387dee3c3510ac349992
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-04-13 22:59:03 -0300

    Styled.

---
+++ styled-components/src/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { Container, Title, SubTitle } from './styles'
+
+function Home() {
+  return (
+    <Container>
+      <Title>Sujeito Programador</Title>
+      <SubTitle>@sujeitoprogramador</SubTitle>
+    </Container>
+  )
+}
+
+export default Home
---
+++ styled-components/src/Home/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #121212;
+`;
+
+export const Title = styled.Text`
+  font-size: 28px;
+  color: #FFF;
+`;
+
+export const SubTitle = styled.Text`
+  font-size: 20px;
+  color: red;
+`;
