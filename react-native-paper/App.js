eff1cf773f6b0fa3dc1471e8358c3d540728b7f6
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-04-15 23:01:33 -0300

    Light/Dark theme.

--- react-native-paper/App.js
+++ react-native-paper/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Appearance } from 'react-native'
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper'
 
 import Home from './src/Home'
@@ -10,14 +11,28 @@ const theme = {
     ...DefaultTheme.colors,
     background: '#FFF',
     primary: '#131313',
+    accent: '#550000',
+    danger: '#ED1C24'
+  }
+}
+
+const dark = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    background: '#131313',
+    primary: '#FFF',
     accent: '#FFD900',
     danger: '#ED1C24'
   }
 }
 
+const colorScheme = Appearance.getColorScheme()
+
 export default function App() {
   return (
-    <PaperProvider theme={theme}>
+    <PaperProvider theme={colorScheme === 'dark' ? dark : theme}>
       <Home />
     </PaperProvider>
   );
--- react-native-paper/src/Home.jsx
+++ react-native-paper/src/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import { Button } from 'react-native-paper'
+import { Button, useTheme } from 'react-native-paper'
 
 export default function Home() {
+  const { colors } = useTheme()
+
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{fontSize: 25}}>Youtube</Text>
-      <Text style={{fontSize: 25}}>Sujeito Programador</Text>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: colors.background}}>
+      <Text style={{fontSize: 25, color: colors.primary}}>Youtube</Text>
+      <Text style={{fontSize: 25, color: colors.accent}}>Sujeito Programador</Text>
       <Button icon="camera" mode="contained" onPress={() => {}}>
         Tirar foto
       </Button>
