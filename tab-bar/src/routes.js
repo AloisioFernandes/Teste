d521c32481c53ce3e45d448959060ce98b8c412a
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-06-08 22:43:16 -0300

    Iniciando Rotas.

---
+++ tab-bar/src/routes.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {} from '@react-navigation/bottom-tabs'
+
+import Home from './pages/Home'
+import New from './pages/New'
+import Notification from './pages/Notification'
+import Profile from './pages/Profile'
+import Search from './pages/Search'
