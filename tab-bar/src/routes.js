08e019fba5ea371723b1d47272ecba45a7f55e3f
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-06-09 23:03:53 -0300

    Navegação por abas.

--- tab-bar/src/routes.js
+++ tab-bar/src/routes.js
@@ -1,8 +1,22 @@
 import React from 'react'
-import {} from '@react-navigation/bottom-tabs'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import Home from './pages/Home'
 import New from './pages/New'
 import Notification from './pages/Notification'
 import Profile from './pages/Profile'
 import Search from './pages/Search'
+
+const Tab = createBottomTabNavigator()
+
+export default function Routes() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Início" component={Home} />
+      <Tab.Screen name="Procurar" component={Search} />
+      <Tab.Screen name="Novo" component={New} />
+      <Tab.Screen name="Notificações" component={Notification} />
+      <Tab.Screen name="Perfil" component={Profile} />
+    </Tab.Navigator>
+  )
+}
