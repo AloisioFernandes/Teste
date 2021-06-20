5daf3a7a638a8ffffeba7615a18e554f9c784419
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-06-20 12:52:29 -0300

    Página detail.

--- alugue/src/pages/Detail.jsx
+++ alugue/src/pages/Detail.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Text, View, Image, TouchableOpacity, StyleSheet } from 'react-native'
 import { Feather, Ionicons } from '@expo/vector-icons'
 import { ScrollView } from 'react-native-gesture-handler'
+import Stars from 'react-native-stars'
 
 import SwiperComponent from '../components/Swiper'
 
@@ -14,9 +15,52 @@ export default function Detail() {
 
       <View style={styles.headerContent}>
         <View style={{ width: '65%' }}>
-          <Text>Casa de Praia</Text>
+          <Text style={styles.house}>Casa de Praia</Text>
+        </View>
+
+        <View style={{ width: '35%' }}>
+          <Text style={styles.rating}>Avaliações</Text>
+          <View style={{ alignItems: 'center', flexDirection: 'row' }}>
+            <Stars
+              default={4}
+              count={5}
+              half={true}
+              starSize={20}
+              fullStar={ <Ionicons name="md-star" size={24} style={styles.myStarStyle} /> }
+              emptyStar={ <Ionicons name="md-star-outline" size={24} style={styles.myStarStyle} /> }
+              halfStar={ <Ionicons name="md-star-half" size={24} style={styles.myStarStyle} /> }
+            />
+          </View>
         </View>
       </View>
+
+      <Text style={styles.price}>R$ 1.200,20</Text>
+      <Text style={styles.description}>
+        Casa nova uma quadra do mar, lugar seguro e monitorado 24 horas.
+      </Text>
+
+      <ScrollView horizontal showsHorizontalScrollIndicator={false} style={{ paddingHorizontal: 15, marginTop: 35 }}>
+        <View style={styles.slide}>
+          <Image
+            source={require('../assets/house5.jpg')}
+            style={{ width: 90, height: 90 }} 
+          />
+        </View>
+
+        <View style={styles.slide}>
+          <Image
+            source={require('../assets/house5.jpg')}
+            style={{ width: 90, height: 90 }} 
+          />
+        </View>
+
+        <View style={styles.slide}>
+          <Image
+            source={require('../assets/house5.jpg')}
+            style={{ width: 90, height: 90 }} 
+          />
+        </View>
+      </ScrollView>
     </View>
   )
 }
@@ -31,7 +75,42 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     height: 340,
     width: '100%'
+  },
+
+  headerContent: {
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    marginTop: 20
+  },
+
+  house: {
+    fontFamily: 'Montserrat_700Bold',
+    fontSize: 18,
+    color: '#4f4a4a'
+  },
+
+  rating: {
+    fontFamily: 'Montserrat_500Medium',
+    fontSize: 9,
+    color: '#4f4a4a'
+  },
+
+  myStarStyle: {
+    color: '#E7A74e',
+    backgroundColor: 'transparent',
+    textShadowColor: '#000',
+    textShadowOffset: { width: 1, height: 1 },
+    textShadowRadius: 1
+  },
+  
+  price: {
+    paddingHorizontal: 20,
+    fontFamily: 'Montserrat_700Bold',
+    fontSize: 16,
+    color: '#000'
   }
 })
 
-// 30:00
+// 40:00
