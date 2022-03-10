bdecce39176aee008c47c8227080ebcb5c94df58
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-03-09 23:37:05 -0300

    Consumindo dados do contexto.

--- contextapi/src/contexts/auth.js
+++ contextapi/src/contexts/auth.js
@@ -3,6 +3,17 @@ import React, { createContext, useState } from 'react'
 export const AuthContext = createContext({})
 
 function AuthProvider({ children }) {
+  const [user, setUser] = useState({})
+
+  function signIn(email, password) {
+    if(email !== '' && password !== '') {
+      setUser({
+        email: email,
+        status: "ATIVO"
+      })
+    }
+  }
+
   return (
     <AuthContext.Provider value={{ nome: "MATHEUS SILVA" }}>
       {children}
--- contextapi/src/pages/Pedidos/index.js
+++ contextapi/src/pages/Pedidos/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import { AuthContext } from '../../contexts/auth'
+
 export default function Pedidos() {
+ const { nome } = useContext()
+
  return (
    <View style={styles.container}>
      <Text>Pagina Pedidos</Text>
+     <Text>Nome: {nome}</Text>
    </View>
   );
 }
