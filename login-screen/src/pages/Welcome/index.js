d75c49f13a2036f81bc3cee5efd2d63a378aba73
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-03-14 22:07:10 -0300

    Estrutura da tela inicial.

--- login-screen/src/pages/Welcome/index.js
+++ login-screen/src/pages/Welcome/index.js
@@ -1,10 +1,37 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { 
+  View, 
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity
+} from 'react-native'
 
 export default function Welcome() {
   return (
-    <View>
-      <Text>Tela Bem Vindo</Text>
+    <View style={styles.container}>
+      <View style={styles.containerLogo}>
+        <Image 
+          source={require('../../assets/logo.png')}
+          style={{ width: '100%' }}
+          resizeMode="contain"
+        />
+      </View>
+
+      <View style={styles.containerForm}>
+        <Text style={styles.title}>Monitore, organize seus gastos de qualquer lugar!</Text>
+        <Text style={styles.text}>Faça o login para começar</Text>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Acessar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+
+  }
+})
