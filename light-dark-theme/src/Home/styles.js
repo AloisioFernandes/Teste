0d0e6e91a6db5a7c799ffc5938830dae2a568de6
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-03-23 23:25:27 -0300

    Estilo página inicial.

--- light-dark-theme/src/Home/styles.js
+++ light-dark-theme/src/Home/styles.js
@@ -1,7 +1,21 @@
 import styled from 'styled-components/native'
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  background-color: #FFF;
+  align-items: center;
+  justify-content: center;
+`;
 
-export const Name = styled.Text``;
+export const Name = styled.Text`
+  font-size: 30px;
+  color: #121212;
+`;
 
-export const Description = styled.Text``;
+export const Description = styled.Text`
+  margin-top: 12px;
+  font-style: italic;
+  color: #121212;
+  background-color: #E9E9E9;
+  padding: 8px 20px;
+`;
