73e015dad4b0628539f7440bf90ca876a9480b4b
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-03-30 23:15:19 -0300

    Estrutura básica do projeto.

--- photography-group/src/App.js
+++ photography-group/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="logo">
+        <img src="" alt="" />
+      </div>
+      <div className="file-drop">
+        <div className="text">Arraste a imagem para cá</div>
+      </div>
+      <div className="preview">
+        <div className="image">
+          <img src="" alt="" />
+        </div>
+      </div>
     </div>
+
   );
 }
 
