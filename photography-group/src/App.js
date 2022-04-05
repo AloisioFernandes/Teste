9ebe6520e7290a9117307c2d41a825a792d39bd4
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-04-04 22:27:49 -0300

    Eventos de arrastar.

--- photography-group/src/App.js
+++ photography-group/src/App.js
@@ -1,33 +1,64 @@
-import './App.css';
+import "./App.css";
 
 function App() {
+  const dragEvents = {
+    onDragEnter: (e) => {
+      e.preventDefault();
+    },
+    onDragLeave: (e) => {
+      e.preventDefault();
+    },
+    onDragOver: (e) => {
+      e.preventDefault();
+    },
+    onDrop: (e) => {
+      e.preventDefault();
+    }
+  }
+
   return (
     <div className="container">
       <div className="logo">
         <img src="/photography-group.png" alt="Photography Group" />
       </div>
-      <div className="file-drop">
+      <div
+        className="file-drop"
+      >
         <div className="text">Arraste a imagem para cá</div>
       </div>
       <div className="preview">
         <div className="image">
-          <img src="https://loremflickr.com/600/600?random=1" alt="Lorem Flickr" />
+          <img
+            src="https://loremflickr.com/600/600?random=1"
+            alt="Lorem Flickr"
+          />
         </div>
         <div className="image">
-          <img src="https://loremflickr.com/600/600?random=2" alt="Lorem Flickr" />
+          <img
+            src="https://loremflickr.com/600/600?random=2"
+            alt="Lorem Flickr"
+          />
         </div>
         <div className="image">
-          <img src="https://loremflickr.com/600/600?random=3" alt="Lorem Flickr" />
+          <img
+            src="https://loremflickr.com/600/600?random=3"
+            alt="Lorem Flickr"
+          />
         </div>
         <div className="image">
-          <img src="https://loremflickr.com/600/600?random=4" alt="Lorem Flickr" />
+          <img
+            src="https://loremflickr.com/600/600?random=4"
+            alt="Lorem Flickr"
+          />
         </div>
         <div className="image">
-          <img src="https://loremflickr.com/600/600?random=5" alt="Lorem Flickr" />
+          <img
+            src="https://loremflickr.com/600/600?random=5"
+            alt="Lorem Flickr"
+          />
         </div>
       </div>
     </div>
-
   );
 }
 
