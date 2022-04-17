ce9099be61853404dca2e7ac37b1aeb9f221addd
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-04-17 17:40:53 -0300

    Monitorando mudança de valor.

--- search-results/src/App.js
+++ search-results/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 
 function App() {
+  const handleInputChange = (e) => {
+    e.preventDefault()
+    const { value } = e.target
+    if(value) return
+    
+    console.log(e.target.value)
+  }
+
   return (
     <div className="container">
       <form>
         <label htmlFor="search">Game search</label>
-        <input name="search" id="search" />
+        <input name="search" id="search" onChange={handleInputChange} />
       </form>
       <div className="search-results">
         <ul>
