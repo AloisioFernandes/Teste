640c93ad7bdf90685310976823f23b1f69d4a079
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-04-26 20:35:39 -0300

    Select inicial.

--- dropdown/src/App.js
+++ dropdown/src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    
+    <div className="container">
+      <form>
+        <label htmlFor="state">Estado:</label>
+        <select id="state">
+          <option value="">Selecione um estado...</option>
+        </select>
+        <label htmlFor="city">Cidade:</label>
+        <select id="city">
+          <option value="">Selecione uma cidade...</option>
+        </select>
+      </form>
+    </div>
   );
 }
 
