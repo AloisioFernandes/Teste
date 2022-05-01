24f986039935640bb9b7b326f87fc72a81661a8e
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-05-01 19:58:40 -0300

    Select de estados.

--- dropdown/src/components/Forms/DropdownBrazilianStates/index.js
+++ dropdown/src/components/Forms/DropdownBrazilianStates/index.js
@@ -1,15 +1,23 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 import { fetchStates } from '../../../helpers/ibge'
 
 const DropdownBrazilianStates = () => {
+  const [states, setStates] = useState([])
+
   useEffect(() => {
-    fetchStates().then()
+    fetchStates().then((states) => {
+      setStates(states)
+    })
   }, [])
 
   return (
     <select id="state">
       <option value="">Selecione um estado...</option>
+      {states.map((state) => {
+        const {sigla, nome} = state
+        return (<option key={sigla} value={sigla}>{nome}</option>)
+      })}
     </select>
   )
 }
