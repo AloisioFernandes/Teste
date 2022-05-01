e03e026de892908033abc38959a9247bc3ad0f05
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-04-30 23:46:18 -0300

    Buscando dados de api.

--- dropdown/src/components/Forms/DropdownBrazilianStates/index.js
+++ dropdown/src/components/Forms/DropdownBrazilianStates/index.js
@@ -1,4 +1,12 @@
+import { useEffect } from 'react'
+
+import { fetchStates } from '../../../helpers/ibge'
+
 const DropdownBrazilianStates = () => {
+  useEffect(() => {
+    fetchStates().then()
+  }, [])
+
   return (
     <select id="state">
       <option value="">Selecione um estado...</option>
