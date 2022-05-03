b1d42acfd11a1bce156e87da98e71e559bba58ac
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-05-02 23:45:54 -0300

    Busca por cidades.

--- dropdown/src/helpers/ibge.js
+++ dropdown/src/helpers/ibge.js
@@ -4,3 +4,8 @@ export const fetchStates = () => {
   const url = `${BASE_URL}/localidades/estados`
   return fetch(url).then(response => response.json())
 }
+
+export const fetchCitiesForState = (state) => {
+  const url = `${BASE_URL}/localidades/estados/${state}/municipios`
+  return fetch(url).then(response => response.json())
+}
