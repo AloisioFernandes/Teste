4d91bf2f5adab35b415a540c2a08a357292d6ea0
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-05-03 23:53:06 -0300

    Resultados da seleção de cidades.

--- dropdown/src/components/Forms/DropdownBrazilianCities/index.js
+++ dropdown/src/components/Forms/DropdownBrazilianCities/index.js
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react"
+
+import { fetchCitiesForState } from "../../../helpers/ibge"
+
 const DropdownBrazilianCities = () => {
+  const [cities, setCities] = useState([])
+
+  useEffect(() => {
+    fetchCitiesForState('MG').then((cities) => {
+      setCities(cities)
+    })
+  }, [])
+
   return (
     <select id="city">
       <option value="">Selecione uma cidade...</option>
+      {cities.map(city) => {
+        return (
+          <option value="">Selecione uma cidade...</option>
+        )
+      }}
     </select>
   )
 }
